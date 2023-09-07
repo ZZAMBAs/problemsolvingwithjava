38ec2bafdb3f4238a7c35aae20242bd41a8812bb
ZZAMBAs <rksidksrksi@naver.com>
2023-09-07 23:49:40 +0900

    SWEA 7396 미해결

---
+++ src/samsungswexpertacademy/Problem7396.java
@@ -0,0 +1,13 @@
+package samsungswexpertacademy;
+
+import java.util.Scanner;
+
+public class Problem7396 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 1; i <= T; i++){
+
+        }
+    }
+}
